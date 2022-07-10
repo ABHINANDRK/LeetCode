052b259db1673fff0783c2b12762a2650312e9bf
ABHINANDRK <abhinandrkvatakara@gmail.com>
2022-07-11 00:01:48 +0530

    solution for validate sodoku problem

---
+++ VaidateSudokuBoard/ValidateSudokuBoard.py
@@ -0,0 +1,57 @@
+# Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+# Each row must contain the digits 1-9 without repetition.
+# Each column must contain the digits 1-9 without repetition.
+# Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+# Note:
+
+# A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+# Only the filled cells need to be validated according to the mentioned rules.
+
+
+# Input: board = 
+# [["5","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]
+# Output: true
+
+class Sudoku:
+    def __init__(self,board):
+        self.board = board
+
+    def test(self):
+
+        big = set()
+        for i in range(0,9):
+            for j in range(0,9):
+                if board[i][j]!='.':
+                    cur = board[i][j]
+                    big.add((i,cur))
+                    big.add((cur,j))
+                    big.add((i/3,j/3,cur))
+        print(big)
+
+
+
+
+
+
+if __name__ == '__main__':
+    board = [["5","3",".",".","7",".",".",".","."]
+    ,["6",".",".","1","9","5",".",".","."]
+    ,[".","9","8",".",".",".",".","6","."]
+    ,["8",".",".",".","6",".",".",".","3"]
+    ,["4",".",".","8",".","3",".",".","1"]
+    ,["7",".",".",".","2",".",".",".","6"]
+    ,[".","6",".",".",".",".","2","8","."]
+    ,[".",".",".","4","1","9",".",".","5"]
+    ,[".",".",".",".","8",".",".","7","9"]]
+    sudoku = Sudoku(board)
+    # print(sudoku.isSudokuBoardValid())
+    print(sudoku.test())
